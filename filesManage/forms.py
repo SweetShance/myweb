722377c076a71808d3049c5c8e585bc3e3929c31
shance <2391454874@qq.com>
2019-04-09 01:55:00 +0800

    登录/注册

--- filesManage/forms.py
+++ filesManage/forms.py
@@ -1,5 +1,46 @@
 from django import forms
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-    user_name = forms.CharField(label="用户名", max_length=20, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '输入用户名'}))
-    password = forms.CharField(label="密码", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '输入密码'}))
+    username = forms.CharField(label="用户名", max_length=20, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '用户名'}))
+    password = forms.CharField(label="密码", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '密码'}))
+
+    # 验证数据
+    def clean(self):
+        # 接收数据
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is None:
+            raise forms.ValidationError("用户名或密码不正确")
+        
+        self.cleaned_data["user"] = user
+        return self.cleaned_data
+
+class RegisterForm(forms.Form):
+    username=forms.CharField(label="用户名", max_length=30, min_length=6, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '用户名'}))
+    password = forms.CharField(label="密码", max_length=30, min_length=6, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '输入密码'}))
+    password_again = forms.CharField(label="再次输入密码", max_length=30, min_length=6, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '再次输入密码'}))
+    email = forms.EmailField(label="邮箱", widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': '输入邮箱'}))
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        # 如果存在
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("用户名已存在")
+        return username
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('邮箱已被注册')
+        return email
+    
+    def clean_password_again(self):
+        password = self.cleaned_data['password']
+        password_again = self.cleaned_data['password_again']
+        if password != password_again:
+            raise forms.ValidationError('两次输入密码不一致')
+        return password
--- filesManage/views.py
+++ filesManage/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
-from .forms import LoginForm
+from django.contrib import auth
+from django.contrib.auth.models import User
+from .forms import LoginForm, RegisterForm
+
 
 def index(request):
-    loginform = LoginForm()
+    # 注册
+    if request.method == "POST":
+        registerform = RegisterForm(request.POST)
+        if registerform.is_valid():
+            username = registerform.cleaned_data['username']
+            password = registerform.cleaned_data['password']
+            email = registerform.cleaned_data['email']
+
+            # 创建用户
+            User.objects.create_user(username=username, email=email, password=password)
+            # 登录
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+    #登录
+    if request.method == "POST":
+        # 接收数据
+        loginform = LoginForm(request.POST)
+        if loginform.is_valid():
+            user = loginform.cleaned_data["user"]
+            auth.login(request, user)
+    else:
+        loginform = LoginForm()
+        registerform = RegisterForm()
     context = {
         "loginform": loginform,
-        "a": "a"
+        "registerform": registerform,
     }
     return render(request, 'base.html', context)
